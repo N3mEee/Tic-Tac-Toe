757517a3126f70160c114d2d965816247ac0db6e
N3mEee <n3mebusiness@gmail.com>
2022-11-25 21:24:55 +0100

    Add winner check function

--- index.js
+++ index.js
@@ -1,71 +1,109 @@
+"use strict";
+
 // gameBoard module
 const gameBoard = (() => {
-    const $gameCanvas = document.querySelector(".gameBoard");
-
     let gameBoard = ["", "", "", "", "", "", "", "", ""];
-    let counter = 0;
 
-    const setGameBoard = (position, value) => {
-        gameBoard[position] = value;
-        displayGameBoard.updateCanvas();
+    const valueAt = (position) => {
+        return gameBoard[position];
     };
 
-    $gameCanvas.addEventListener("click", (e) => {
-        if (e.target.textContent === "") {
-            if (counter % 2 === 0) {
-                setGameBoard(e.target.dataset.id, "x");
-            } else {
-                setGameBoard(e.target.dataset.id, "0");
-            }
-            counter++;
-        }
-    });
-    return { gameBoard };
+    const updateGameBoard = (position, value) => {
+        gameBoard[position] = value;
+        displayGameBoard.updateCells();
+    };
+
+    const getGameBoard = function () {
+        return gameBoard;
+    };
+
+    return { valueAt, getGameBoard, updateGameBoard };
 })();
 
 // player factory function
 const player = (name) => {
     const playerName = name;
-    let score = 0;
     const getPlayerName = () => {
         return playerName;
     };
-    const getPlayerScore = () => {
-        return score;
-    };
-    const setPlayerScore = (value) => {
-        score += value;
-    };
 
-    return { getPlayerName, getPlayerScore };
+    return { getPlayerName };
 };
 
 // game module
 const game = ((name1, name2) => {
+    const $gameBoard = document.querySelector(".gameBoard");
     const player1 = player(name1);
     const player2 = player(name2);
-    const showWinner = () => {
-        console.log(`${getPlayerName()} won`);
-        setPlayerScore(1);
+    let round = 0;
+
+    const checkWinner = function () {
+        const winnerCombos = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        const gameWon = () => winnerCombos.some((combo) => allSameSymbol(combo));
+        const gameTie = () => {
+            if (!gameBoard.getGameBoard().includes("") && !gameWon()) {
+                return true;
+            }
+        };
+        function allSameSymbol(combo) {
+            return combo.every((item) => {
+                if (round % 2 === 0) {
+                    return gameBoard.valueAt(item) === "x";
+                } else {
+                    return gameBoard.valueAt(item) === "0";
+                }
+            });
+        }
+        if (gameWon()) {
+            console.log("winner");
+        } else if (gameTie()) {
+            console.log("tie");
+        } else {
+            console.log("no winner yet");
+        }
     };
-    return { player1, player2 };
+
+    $gameBoard.addEventListener("click", (e) => {
+        if (e.target.textContent === "") {
+            if (round % 2 === 0) {
+                gameBoard.updateGameBoard(e.target.dataset.id, "x");
+                checkWinner();
+            } else {
+                gameBoard.updateGameBoard(e.target.dataset.id, "0");
+                checkWinner();
+            }
+            round++;
+        }
+    });
+
+    return { player1, player2, checkWinner };
 })("Marcel", "Caramel");
 
 // displayGameBoard module
 const displayGameBoard = (() => {
-    const $gameCanvas = document.querySelector(".gameBoard");
-    for (let i = 0; i < gameBoard.gameBoard.length; i++) {
-        const $div = document.createElement("div");
-        $div.setAttribute("class", "box");
-        $div.setAttribute("data-id", i);
-        $div.textContent = gameBoard.gameBoard[i];
-        $gameCanvas.appendChild($div);
+    const $gameBoard = document.querySelector(".gameBoard");
+    for (let i = 0; i < gameBoard.getGameBoard().length; i++) {
+        const $cell = document.createElement("div");
+        $cell.setAttribute("class", "cell");
+        $cell.setAttribute("data-id", i);
+        $cell.textContent = gameBoard.getGameBoard()[i];
+        $gameBoard.appendChild($cell);
     }
 
-    const updateCanvas = () => {
-        for (const child of $gameCanvas.children) {
-            child.textContent = gameBoard.gameBoard[child.dataset.id];
+    const updateCells = () => {
+        for (const child of $gameBoard.children) {
+            child.textContent = gameBoard.getGameBoard()[child.dataset.id];
         }
     };
-    return { updateCanvas };
+    return { updateCells };
 })();
